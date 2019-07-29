6aa53d0019dffc944ac2332b53298b39c0d4674a
Taran Cacacho <tarancacacho@gmail.com>
2019-07-29 12:48:48 -0700

    Create reducing actions for receive single and multiple todos

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,5 +1,35 @@
-const todosReducer = (state, action) => {
+import { RECEIVE_TODO, RECEIVE_TODOS} from '../actions/todo_actions';
+import merge from 'lodash/merge';
+
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let newState;
   switch(action.type) {
+    case RECEIVE_TODOS:
+      let todos = {};
+      action.todos.map((todo) => {
+        todos[todo.id] = todo;
+      })
+      newState = merge({}, todos);
+      return newState;
+    case RECEIVE_TODO:
+      newState = merge({}, initialState, {[action.todo.id]: action.todo});
+      return newState;
     default:
       return state;
   }
