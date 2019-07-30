f82fee5db3dc1a3d74dd9718a83d734e3eaafd24
Taran Cacacho <tarancacacho@gmail.com>
2019-07-30 00:44:26 -0700

    Create stepsReducer, write default case

---
+++ frontend/reducers/steps_reducer.js
@@ -0,0 +1,10 @@
+import merge from 'lodash/merge';
+
+const stepsReducer = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default stepsReducer;
