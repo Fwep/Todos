46775643174bc9aa99a37bb5bcc7215dd697e349
Taran Cacacho <tarancacacho@gmail.com>
2019-07-29 23:58:09 -0700

    Refactor component to class form, and only render title and status toggler button

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import merge from 'lodash/merge';
+import TodoDetailViewContainer from './todo_detail_view_container';
 
-const TodoListItem = ({ todo, removeTodo, receiveTodo }) => (
-  <li>
-    <h3>{todo.title}</h3>
-    <h4>{todo.body}</h4>
-    <button onClick={() => removeTodo(todo.id)}>Delete Todo</button>
-    <button onClick={() => receiveTodo(Object.assign(todo, {done: !todo.done}))}>{todo.done ? 'Undo' : 'Done'}</button>
-  </li>
-);
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      detail: false
+    }
+
+    this.toggleDetail = this.toggleDetail.bind(this);
+  }
+
+  toggleDetail(e) {
+    e.preventDefault();
+    this.setState(merge({}, this.state, {detail: !this.state.detail}));
+  }
+
+  render() {
+    let {receiveTodo, todo} = this.props
+    return (
+      <li onClick={this.toggleDetail}>
+        <h3>{todo.title}</h3>
+        <button onClick={() => receiveTodo(Object.assign(todo, { done: !todo.done }))}>{todo.done ? 'Undo' : 'Done'}</button>
+        {this.state.detail && <TodoDetailViewContainer todo={todo} />}
+      </li>
+    )
+  }
+}
 
 export default TodoListItem;
