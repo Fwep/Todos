10f6b105c4cd777dfc0da6907606dbd8ca3731df
Taran Cacacho <tarancacacho@gmail.com>
2019-07-29 20:54:02 -0700

    Add logic to remove and toggle status of individual todo

--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const TodoListItem = ({ todo }) => (
+const TodoListItem = ({ todo, removeTodo, receiveTodo }) => (
   <li>
     <h3>{todo.title}</h3>
     <h4>{todo.body}</h4>
+    <button onClick={() => removeTodo(todo.id)}>Delete Todo</button>
+    <button onClick={() => receiveTodo(Object.assign(todo, {done: !todo.done}))}>{todo.done ? 'Undo' : 'Done'}</button>
   </li>
 );
 
