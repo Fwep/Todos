ca05380d462c2b19c938fd2d40ff25774c92940c
Taran Cacacho <tarancacacho@gmail.com>
2019-07-29 20:54:33 -0700

    Add todo removal to mDP

--- frontend/components/todos/todo_list_container.jsx
+++ frontend/components/todos/todo_list_container.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TodoList from './todo_list';
 import { allTodos } from '../../reducers/selectors';
-import { receiveTodo } from '../../actions/todo_actions';
+import { receiveTodo, removeTodo } from '../../actions/todo_actions';
 
 const mSP = (state) => ({
   todos: allTodos(state)
 });
 
 const mDP = (dispatch) => ({
-  receiveTodo: (todo) => dispatch(receiveTodo(todo))
+  receiveTodo: (todo) => dispatch(receiveTodo(todo)),
+  removeTodo: (id) => dispatch(removeTodo(id))
 })
 
 export default connect(mSP, mDP)(TodoList);
