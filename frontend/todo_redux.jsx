7ac66d5cda9d94c6d8f8926a287dc18fc8283eeb
Taran Cacacho <tarancacacho@gmail.com>
2019-07-30 17:25:40 -0700

    Remove testing statements

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import store from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-
   const root = document.getElementById('root')
-  ReactDOM.render(<h1>Bless the lawd!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
