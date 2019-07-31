bb4a0745c25beda259c9e6abadce0816f2fadec1
Taran Cacacho <tarancacacho@gmail.com>
2019-07-30 18:00:30 -0700

    Configure store to hook up to localStorage and load from there if state is present

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-import store from './store/store';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root')
+  const preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
+  const root = document.getElementById('root');
+  const store = configureStore(preloadedState);
   ReactDOM.render(<Root store={store} />, root);
 });
