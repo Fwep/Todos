796ba626dc473007bc19e1b1d22c5b0ed4bbe513
Taran Cacacho <tarancacacho@gmail.com>
2019-07-30 17:24:56 -0700

    Add receiving and removing of steps to reducer

--- frontend/reducers/steps_reducer.js
+++ frontend/reducers/steps_reducer.js
@@ -1,7 +1,44 @@
 import merge from 'lodash/merge';
+import { 
+  RECEIVE_STEPS, 
+  RECEIVE_STEP,
+  REMOVE_STEP
+ } from '../actions/step_actions';
 
-const stepsReducer = (state = [], action) => {
+const initialState = {
+  1: { // this is the step with id = 1
+    id: 1,
+    title: 'walk to store',
+    body: 'CVS',
+    done: false,
+    todo_id: 1
+    },
+  2: { // this is the step with id = 2
+    id: 2,
+    title: 'buy soap',
+    body: 'Irish Spring',
+    done: false,
+    todo_id: 1
+    }
+}
+
+const stepsReducer = (state = initialState, action) => {
+  Object.freeze(state);
+  let newState = {};
+
   switch (action.type) {
+    case RECEIVE_STEPS:
+      action.steps.forEach(step => {
+        newState[step.id] = step;
+      })
+      return newState;
+    case RECEIVE_STEP:
+      newState = merge({}, state, {[action.step.id]: action.step});
+      return newState;
+    case REMOVE_STEP:
+      newState = merge({}, state);
+      delete newState[action.step.id];
+      return newState;
     default:
       return state;
   }
