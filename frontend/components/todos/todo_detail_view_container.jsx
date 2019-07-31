ad9d56a3ee99f4fbcb5a9cd92d39a5c6ee3c7b3f
Taran Cacacho <tarancacacho@gmail.com>
2019-07-30 17:23:32 -0700

    Import and add receiveSteps to mSP

--- frontend/components/todos/todo_detail_view_container.jsx
+++ frontend/components/todos/todo_detail_view_container.jsx
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import { removeTodo } from '../../actions/todo_actions';
+import { receiveSteps } from '../../actions/step_actions';
 import TodoDetailView from './todo_detail_view';
 
-const mDP = (dispatch) => ({
-  removeTodo: (id) => dispatch(removeTodo(id))
+const mDP = (dispatch, { todo }) => ({
+  removeTodo: () => dispatch(removeTodo(todo.id)),
+  receiveSteps: (steps) => dispatch(receiveSteps(steps))
 })
 
 export default connect(null, mDP)(TodoDetailView)
