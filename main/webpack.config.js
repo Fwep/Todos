51ba2fc6dcca1217d8c4cf487809029ef38f0afa
Taran Cacacho <tarancacacho@gmail.com>
2019-08-05 20:34:36 -0700

    Restructure file tree

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
 
 module.exports = {
+  context: __dirname,
   entry: './frontend/todo_redux.jsx',
   output: {
-    filename: './bundle.js'
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
   },
   devtool: 'source-map',
   resolve: {
